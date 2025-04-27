1634dec1d01fb155347df88867caf7a38af0a167
juliacalvert <147757218+juliacalvert@users.noreply.github.com>
2025-04-27 14:58:02 -0400

    stocks page

--- stocks.js
+++ stocks.js
@@ -1,4 +1,41 @@
+async function redditStocks() {
+    const url = "https://tradestie.com/api/v1/apps/reddit?date=2022-04-03";
+    const response = await fetch(url);
+    const stocks = await response.json();
+    const top5 = stocks.slice(0, 5);
+
+    const tableBody = document.querySelector('#reddit-stocks tbody');
+    tableBody.innerHTML = '';
+
+    top5.forEach((stock) => {
+        const tableRow = document.createElement('tr');
+        const tickerCell = document.createElement('td');
+        const link = document.createElement('a');
+
+        link.href = `https://finance.yahoo.com/quote/${stock.ticker}`;
+        link.textContent = stock.ticker;
+        tickerCell.appendChild(link);
+
+        const commentsCell = document.createElement('td');
+        commentsCell.textContent = stock.no_of_comments;
+
+        const sentimentCell = document.createElement('td');
+        sentimentCell.innerHTML = stock.sentiment === "Bullish" ? "🐂 Bullish" : "🐻 Bearish";
+
+        tableRow.appendChild(tickerCell);
+        tableRow.appendChild(commentsCell);
+        tableRow.appendChild(sentimentCell);
+
+        tableBody.appendChild(tableRow);
+
+    
+      });
+}
+
+
 window.onload = function () {
+    redditStocks();
+
     const ctx = document.getElementById('myChart').getContext('2d');
 
     new Chart(ctx, {
@@ -20,3 +57,4 @@ window.onload = function () {
       }
     });
   };
+
